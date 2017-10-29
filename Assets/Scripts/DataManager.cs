95d0705591af1a8c6fa5f270830034cec434a2c1
Chapa.C(Takuya) <chapatown@gmail.com>
2017-10-29 23:18:05 +0900

    G2Uの導入

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using Google2u;
+
+/// <summary>
+/// データ管理クラス
+/// </summary>
+public class DataManager : SingletonMonoBehaviour<DataManager>
+{
+	[SerializeField] private EnemyMaster m_enemyMaster;
+
+	/// <summary>
+	/// 敵データをマスターから取得
+	/// </summary>
+	public EnemyMasterRow GetEnemyDataInMaster(string identifier)
+	{
+		return m_enemyMaster.GetRow(identifier);
+	}
+}
