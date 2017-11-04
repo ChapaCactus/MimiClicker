92ab033a3368f8d667ba9ddc53a26e8a6d47adf0
Chapa.C(Takuya) <chapatown@gmail.com>
2017-11-04 15:57:30 +0900

    アイテムVO, DTOを更新

---
+++ Assets/Scripts/DTO/ItemDTO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using MMCL.VO;
+
+namespace MMCL.DTO
+{
+	[Serializable]
+	public class ItemDTO
+	{
+		public enum RarityEnum
+		{
+			N,
+			R,
+			SR,
+			SSR,
+			SSSR,
+			SSSSR
+		}
+
+		private ItemVO m_vo;
+
+		public bool IsUnique { get { return m_vo.IsUnique; } }
+		public string Name { get { return m_vo.Name; } }
+		public RarityEnum Rarity { get { return (RarityEnum)m_vo.Rarity; } }
+		public int Quantity { get { return m_vo.Quantity; } }
+		public string Description { get { return m_vo.Description; } }
+
+		public void SetVO(ItemVO vo)
+		{
+			m_vo = vo;
+		}
+	}
+}
---
+++ Assets/Scripts/VO/ItemVO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MMCL.VO
+{
+	[Serializable]
+	public class ItemVO
+	{
+		// ユニークアイテムか？
+		public bool IsUnique { get; set; }
+		public string Name { get; set; }
+		public int Rarity { get; set; }
+		public int Quantity { get; set; }
+		public string Description { get; set; }
+	}
+}
